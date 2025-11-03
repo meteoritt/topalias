13f2581b060b18d231c2ee58b449126b19907238
Sergey Chudakov <csredrat@gmail.com>
2025-11-03 07:16:25 +0300

    Update version topalias to 3.2.0 \

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/CSRedRat/topalias",
-    version="3.0.0",
+    version="3.2.0",
     zip_safe=False,
 )
--- topalias/__init__.py
+++ topalias/__init__.py
@@ -6,7 +6,7 @@ import sys
 
 __author__ = "Sergey Chudakov"
 __email__ = "csredrat@gmail.com"
-__version__ = "3.0.0"
+__version__ = "3.2.0"
 
 sys.path.insert(
     0,
